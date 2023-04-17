6c0a5de9a938e5cf3393435535f150dbc69b3475
Arielgsp <campa990@gmail.com>
2023-04-17 20:35:22 -0300

    Blackjack Game-Alone finished

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ let btnAlone = document.getElementById("buttonAlone");
 let restNumber = document.getElementById("restNumber");
 let pDisplay = document.getElementById("pDisplay");
 let btnAloneReset = document.getElementById("buttonAloneReset");
+let btnAloneResetWin = document.getElementById("buttonAloneResetWin");
+let cardOne = document.getElementById("cardOne");
 let cardRandomNumber;
 let restNumber0;
 let resultTotal;
 let arrayCards = [];
-
+let messageLost = document.getElementById("messageLost");
+let messageWin = document.getElementById("messageWin");
 restNumber.innerHTML = 21;
 function cardAloneNumber() {
   cardRandomNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
@@ -17,6 +20,8 @@ function cardAloneNumber() {
 
 function pushCards() {
   arrayCards.push(cardRandomNumber);
+  cardOne.innerHTML =
+    "Te salió la carta Número: " + arrayCards[arrayCards.length - 1];
 
   console.log(arrayCards);
   resultTotal = arrayCards.reduce((a, b) => a + b, 0);
@@ -37,21 +42,37 @@ function pushCards() {
     cardAlone.innerHTML += arrayCards[arrayCards.length - 1];
     pDisplay.innerHTML = "GANASTE!";
     pDisplay.style.color = "darkgreen";
+    messageWin.style.display = "inline";
   } else if (restNumber0 < 0) {
     cardAlone.innerHTML += arrayCards[arrayCards.length - 1];
     pDisplay.innerHTML = "PERDISTE!";
     pDisplay.style.color = "darkred";
+    messageLost.style.display = "inline";
   }
 
   console.log(resultTotal);
 }
 
 btnAloneReset.addEventListener("click", () => {
-  cardAlone.innerHTML = " ";
+  cardAlone.innerHTML = "Cartas Totales: ";
   pDisplay.innerHTML = " ";
   arrayCards = [];
   restNumber0 = 21 - resultTotal;
   restNumber.innerHTML = 21;
+  messageLost.style.display = "none";
+  console.log("prueba");
+  cardOne.innerHTML = "Te salió la carta Número: ";
+});
+
+btnAloneResetWin.addEventListener("click", () => {
+  cardAlone.innerHTML = "Cartas Totales: ";
+  pDisplay.innerHTML = " ";
+  arrayCards = [];
+  restNumber0 = 21 - resultTotal;
+  restNumber.innerHTML = 21;
+  messageWin.style.display = "none";
+  console.log("prueba");
+  cardOne.innerHTML = "Te salió la carta Número: ";
 });
 
 /*
